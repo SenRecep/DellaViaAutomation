00d8fd99b44076cfa5151ac24a7cc6ec13c76e76
recep şen <recep-sen-200013@hotmail.com>
2019-07-24 17:29:51 +0300

    Test

---
+++ DellaViaAutomation.Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DellaViaAutomation.Entities
+{
+    public class Order
+    {
+        public int id { get; set; }
+    }
+}
